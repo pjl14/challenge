c0f88fdd748714a8d038f32bec0fcdc08a2d2565
Santiago Rojas <pjl14@outlook.com>
2020-10-03 12:43:35 -0500

    commint

--- task_02/02bash_create-delete-VM.sh
+++ task_02/02bash_create-delete-VM.sh
@@ -9,13 +9,13 @@ if [[ $Option -eq 1 ]]
 then
 #Create instance
     echo "Creating instance..."
-    gcloud compute instances create rhel-7-v20200910 --zone southamerica-east1-a
+    gcloud compute instances create centos-7-v20200910 --zone southamerica-east1-a
  echo "done"
 elif [[ $Option -eq 2 ]]
 then
 #delete instance
     echo "Deleting instance..."
-    gcloud compute instances delete rhel-7-v20200910 --zone southamerica-east1-a
+    gcloud compute instances delete centos-7-v20200910 --zone southamerica-east1-a
  echo "done"
 fi
 

a7e53142a5c122bb15ab8fa3b28cb5d21d75ea47
Santiago Rojas <pjl14@outlook.com>
2020-10-03 12:37:57 -0500

    commint

---
+++ task_01/01bash_create-delete-VM.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Option 1 = Create instence"
+echo "Option 2 = Delete instence"
+echo -n "Enter Option: "
+read Option
+
+if [[ $Option -eq 1 ]]
+then
+#Create instance
+    echo "Creating instance..."
+    gcloud compute instances create rhel-7-v20200910 --zone southamerica-east1-a
+ echo "done"
+elif [[ $Option -eq 2 ]]
+then
+#delete instance
+    echo "Deleting instance..."
+    gcloud compute instances delete rhel-7-v20200910 --zone southamerica-east1-a
+ echo "done"
+fi
+

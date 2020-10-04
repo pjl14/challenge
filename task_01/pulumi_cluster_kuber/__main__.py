286d44aa11db9e22cb9ee7e9c0577c0c34f8a8f9
Santiago Rojas <pjl14@outlook.com>
2020-10-03 19:37:49 -0500

    commit

--- task_01/pulumi_cluster_kuber/__main__.py
+++ task_01/pulumi_cluster_kuber/__main__.py
@@ -40,7 +40,7 @@ machine_type = 'g1-small'
 
 cluster = container.Cluster(
     cluster_name,
-    initial_node_count=3,
+    initial_node_count=1,
     min_master_version='latest',
     node_version='latest',
     node_config={
